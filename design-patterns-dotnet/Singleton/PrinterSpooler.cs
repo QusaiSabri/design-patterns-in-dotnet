19239984f372c305ddcce8c56a85eaba088a05c8
Qusai Sabri <qusai.sabri19@gmail.com>
2024-01-01 23:17:14 -0600

    Added PrinterSpooler & PrinterSpoolerSingleton and restructured classes

---
+++ design-patterns-dotnet/Singleton/PrinterSpooler.cs
@@ -0,0 +1,33 @@
+﻿
+public class PrinterSpooler
+{
+
+    public void Print(string document)
+    {
+        for (int i = 1; i <= 3; i++)
+        {
+            Console.WriteLine($"Printing page {i} of {document}");
+            Thread.Sleep(100);
+        }
+        Console.WriteLine(" ");
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        var spooler1 = new PrinterSpooler();
+        var spooler2 = new PrinterSpooler();
+
+
+        Thread thread1 = new Thread(() => spooler1.Print("Document 1"));
+        Thread thread2 = new Thread(() => spooler2.Print("Document 2"));
+
+        thread1.Start();
+        thread2.Start();
+
+        thread1.Join();
+        thread2.Join();
+    }
+}
---
+++ design-patterns-dotnet/Singleton/PrinterSpoolerSingleton.cs
@@ -0,0 +1,53 @@
+﻿//public class PrinterSpoolerSingleton
+//{
+//    private static PrinterSpoolerSingleton? _instance;
+//    private static readonly object instanceLock = new();
+
+//    private PrinterSpoolerSingleton() { }
+
+//    public static PrinterSpoolerSingleton GetInstance()
+//    {
+//        if (_instance == null)
+//        {
+//            lock (instanceLock)
+//            {
+//                if (_instance == null)
+//                {
+//                    _instance = new PrinterSpoolerSingleton();
+//                }
+//            }
+//        }
+//        return _instance;
+//    }
+
+//    public void Print(string document)
+//    {
+//        lock (instanceLock)
+//        {
+//            for (int i = 1; i <= 3; i++)
+//            {
+//                Console.WriteLine($"Printing page {i} of {document}");
+//                Thread.Sleep(100);
+//            }
+//        }
+
+//        Console.WriteLine(" ");
+//    }
+//}
+
+//public class Program
+//{
+//    public static void Main()
+//    {
+//        var spooler = PrinterSpoolerSingleton.GetInstance();
+
+//        Thread thread1 = new Thread(() => spooler.Print("Document 1"));
+//        Thread thread2 = new Thread(() => spooler.Print("Document 2"));
+
+//        thread1.Start();
+//        thread2.Start();
+
+//        thread1.Join();
+//        thread2.Join();
+//    }
+//}
